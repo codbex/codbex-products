2d50bed973aea08cfa9a61f6f520cda107450b61
MinaDoncheva <mina.doncheva@icloud.com>
2024-08-30 12:03:10 +0300

    make ratio part of SetProduct entity

---
+++ codbex-products/gen/codbex-products/api/Products/ProductSetService.ts
@@ -0,0 +1,162 @@
+import { Controller, Get, Post, Put, Delete, response } from "sdk/http"
+import { Extensions } from "sdk/extensions"
+import { ProductSetRepository, ProductSetEntityOptions } from "../../dao/Products/ProductSetRepository";
+import { ValidationError } from "../utils/ValidationError";
+import { HttpUtils } from "../utils/HttpUtils";
+
+const validationModules = await Extensions.loadExtensionModules("codbex-products-Products-ProductSet", ["validate"]);
+
+@Controller
+class ProductSetService {
+
+    private readonly repository = new ProductSetRepository();
+
+    @Get("/")
+    public getAll(_: any, ctx: any) {
+        try {
+            const options: ProductSetEntityOptions = {
+                $limit: ctx.queryParameters["$limit"] ? parseInt(ctx.queryParameters["$limit"]) : undefined,
+                $offset: ctx.queryParameters["$offset"] ? parseInt(ctx.queryParameters["$offset"]) : undefined
+            };
+
+            let Product = parseInt(ctx.queryParameters.Product);
+            Product = isNaN(Product) ? ctx.queryParameters.Product : Product;
+
+            if (Product !== undefined) {
+                options.$filter = {
+                    equals: {
+                        Product: Product
+                    }
+                };
+            }
+
+            return this.repository.findAll(options);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Post("/")
+    public create(entity: any) {
+        try {
+            this.validateEntity(entity);
+            entity.Id = this.repository.create(entity);
+            response.setHeader("Content-Location", "/services/ts/codbex-products/gen/codbex-products/api/Products/ProductSetService.ts/" + entity.Id);
+            response.setStatus(response.CREATED);
+            return entity;
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Get("/count")
+    public count() {
+        try {
+            return this.repository.count();
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Post("/count")
+    public countWithFilter(filter: any) {
+        try {
+            return this.repository.count(filter);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Post("/search")
+    public search(filter: any) {
+        try {
+            return this.repository.findAll(filter);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Get("/:id")
+    public getById(_: any, ctx: any) {
+        try {
+            const id = parseInt(ctx.pathParameters.id);
+            const entity = this.repository.findById(id);
+            if (entity) {
+                return entity;
+            } else {
+                HttpUtils.sendResponseNotFound("ProductSet not found");
+            }
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Put("/:id")
+    public update(entity: any, ctx: any) {
+        try {
+            entity.Id = ctx.pathParameters.id;
+            this.validateEntity(entity);
+            this.repository.update(entity);
+            return entity;
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Delete("/:id")
+    public deleteById(_: any, ctx: any) {
+        try {
+            const id = ctx.pathParameters.id;
+            const entity = this.repository.findById(id);
+            if (entity) {
+                this.repository.deleteById(id);
+                HttpUtils.sendResponseNoContent();
+            } else {
+                HttpUtils.sendResponseNotFound("ProductSet not found");
+            }
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    private handleError(error: any) {
+        if (error.name === "ForbiddenError") {
+            HttpUtils.sendForbiddenRequest(error.message);
+        } else if (error.name === "ValidationError") {
+            HttpUtils.sendResponseBadRequest(error.message);
+        } else {
+            HttpUtils.sendInternalServerError(error.message);
+        }
+    }
+
+    private validateEntity(entity: any): void {
+        if (entity.Product === null || entity.Product === undefined) {
+            throw new ValidationError(`The 'Product' property is required, provide a valid value`);
+        }
+        if (entity.UoM === null || entity.UoM === undefined) {
+            throw new ValidationError(`The 'UoM' property is required, provide a valid value`);
+        }
+        if (entity.UoM?.length > 20) {
+            throw new ValidationError(`The 'UoM' exceeds the maximum length of [20] characters`);
+        }
+        if (entity.Weight === null || entity.Weight === undefined) {
+            throw new ValidationError(`The 'Weight' property is required, provide a valid value`);
+        }
+        if (entity.Height === null || entity.Height === undefined) {
+            throw new ValidationError(`The 'Height' property is required, provide a valid value`);
+        }
+        if (entity.Length === null || entity.Length === undefined) {
+            throw new ValidationError(`The 'Length' property is required, provide a valid value`);
+        }
+        if (entity.Width === null || entity.Width === undefined) {
+            throw new ValidationError(`The 'Width' property is required, provide a valid value`);
+        }
+        if (entity.Ratio === null || entity.Ratio === undefined) {
+            throw new ValidationError(`The 'Ratio' property is required, provide a valid value`);
+        }
+        for (const next of validationModules) {
+            next.validate(entity);
+        }
+    }
+
+}
---
+++ codbex-products/gen/codbex-products/dao/Products/ProductSetRepository.ts
@@ -0,0 +1,283 @@
+import { query } from "sdk/db";
+import { producer } from "sdk/messaging";
+import { extensions } from "sdk/extensions";
+import { dao as daoApi } from "sdk/db";
+
+export interface ProductSetEntity {
+    readonly Id: number;
+    Product?: number;
+    UoM?: string;
+    Weight: number;
+    Height: number;
+    Length: number;
+    Width: number;
+    Ratio?: number;
+}
+
+export interface ProductSetCreateEntity {
+    readonly Product?: number;
+    readonly UoM?: string;
+    readonly Weight: number;
+    readonly Height: number;
+    readonly Length: number;
+    readonly Width: number;
+    readonly Ratio?: number;
+}
+
+export interface ProductSetUpdateEntity extends ProductSetCreateEntity {
+    readonly Id: number;
+}
+
+export interface ProductSetEntityOptions {
+    $filter?: {
+        equals?: {
+            Id?: number | number[];
+            Product?: number | number[];
+            UoM?: string | string[];
+            Weight?: number | number[];
+            Height?: number | number[];
+            Length?: number | number[];
+            Width?: number | number[];
+            Ratio?: number | number[];
+        };
+        notEquals?: {
+            Id?: number | number[];
+            Product?: number | number[];
+            UoM?: string | string[];
+            Weight?: number | number[];
+            Height?: number | number[];
+            Length?: number | number[];
+            Width?: number | number[];
+            Ratio?: number | number[];
+        };
+        contains?: {
+            Id?: number;
+            Product?: number;
+            UoM?: string;
+            Weight?: number;
+            Height?: number;
+            Length?: number;
+            Width?: number;
+            Ratio?: number;
+        };
+        greaterThan?: {
+            Id?: number;
+            Product?: number;
+            UoM?: string;
+            Weight?: number;
+            Height?: number;
+            Length?: number;
+            Width?: number;
+            Ratio?: number;
+        };
+        greaterThanOrEqual?: {
+            Id?: number;
+            Product?: number;
+            UoM?: string;
+            Weight?: number;
+            Height?: number;
+            Length?: number;
+            Width?: number;
+            Ratio?: number;
+        };
+        lessThan?: {
+            Id?: number;
+            Product?: number;
+            UoM?: string;
+            Weight?: number;
+            Height?: number;
+            Length?: number;
+            Width?: number;
+            Ratio?: number;
+        };
+        lessThanOrEqual?: {
+            Id?: number;
+            Product?: number;
+            UoM?: string;
+            Weight?: number;
+            Height?: number;
+            Length?: number;
+            Width?: number;
+            Ratio?: number;
+        };
+    },
+    $select?: (keyof ProductSetEntity)[],
+    $sort?: string | (keyof ProductSetEntity)[],
+    $order?: 'asc' | 'desc',
+    $offset?: number,
+    $limit?: number,
+}
+
+interface ProductSetEntityEvent {
+    readonly operation: 'create' | 'update' | 'delete';
+    readonly table: string;
+    readonly entity: Partial<ProductSetEntity>;
+    readonly key: {
+        name: string;
+        column: string;
+        value: number;
+    }
+}
+
+interface ProductSetUpdateEntityEvent extends ProductSetEntityEvent {
+    readonly previousEntity: ProductSetEntity;
+}
+
+export class ProductSetRepository {
+
+    private static readonly DEFINITION = {
+        table: "CODBEX_PRODUCTSET",
+        properties: [
+            {
+                name: "Id",
+                column: "PRODUCTSET_ID",
+                type: "INTEGER",
+                id: true,
+                autoIncrement: true,
+            },
+            {
+                name: "Product",
+                column: "PRODUCTSET_PRODUCT",
+                type: "INTEGER",
+            },
+            {
+                name: "UoM",
+                column: "PRODUCTSET_UOM",
+                type: "VARCHAR",
+            },
+            {
+                name: "Weight",
+                column: "PRODUCTSET_WEIGHT",
+                type: "DECIMAL",
+                required: true
+            },
+            {
+                name: "Height",
+                column: "PRODUCTSET_HEIGHT",
+                type: "DECIMAL",
+                required: true
+            },
+            {
+                name: "Length",
+                column: "PRODUCTSET_LENGTH",
+                type: "DECIMAL",
+                required: true
+            },
+            {
+                name: "Width",
+                column: "PRODUCTSET_WIDTH",
+                type: "DECIMAL",
+                required: true
+            },
+            {
+                name: "Ratio",
+                column: "PRODUCTSET_RATIO",
+                type: "DECIMAL",
+            }
+        ]
+    };
+
+    private readonly dao;
+
+    constructor(dataSource = "DefaultDB") {
+        this.dao = daoApi.create(ProductSetRepository.DEFINITION, null, dataSource);
+    }
+
+    public findAll(options?: ProductSetEntityOptions): ProductSetEntity[] {
+        return this.dao.list(options);
+    }
+
+    public findById(id: number): ProductSetEntity | undefined {
+        const entity = this.dao.find(id);
+        return entity ?? undefined;
+    }
+
+    public create(entity: ProductSetCreateEntity): number {
+        const id = this.dao.insert(entity);
+        this.triggerEvent({
+            operation: "create",
+            table: "CODBEX_PRODUCTSET",
+            entity: entity,
+            key: {
+                name: "Id",
+                column: "PRODUCTSET_ID",
+                value: id
+            }
+        });
+        return id;
+    }
+
+    public update(entity: ProductSetUpdateEntity): void {
+        const previousEntity = this.findById(entity.Id);
+        this.dao.update(entity);
+        this.triggerEvent({
+            operation: "update",
+            table: "CODBEX_PRODUCTSET",
+            entity: entity,
+            previousEntity: previousEntity,
+            key: {
+                name: "Id",
+                column: "PRODUCTSET_ID",
+                value: entity.Id
+            }
+        });
+    }
+
+    public upsert(entity: ProductSetCreateEntity | ProductSetUpdateEntity): number {
+        const id = (entity as ProductSetUpdateEntity).Id;
+        if (!id) {
+            return this.create(entity);
+        }
+
+        const existingEntity = this.findById(id);
+        if (existingEntity) {
+            this.update(entity as ProductSetUpdateEntity);
+            return id;
+        } else {
+            return this.create(entity);
+        }
+    }
+
+    public deleteById(id: number): void {
+        const entity = this.dao.find(id);
+        this.dao.remove(id);
+        this.triggerEvent({
+            operation: "delete",
+            table: "CODBEX_PRODUCTSET",
+            entity: entity,
+            key: {
+                name: "Id",
+                column: "PRODUCTSET_ID",
+                value: id
+            }
+        });
+    }
+
+    public count(options?: ProductSetEntityOptions): number {
+        return this.dao.count(options);
+    }
+
+    public customDataCount(): number {
+        const resultSet = query.execute('SELECT COUNT(*) AS COUNT FROM "CODBEX_SET"');
+        if (resultSet !== null && resultSet[0] !== null) {
+            if (resultSet[0].COUNT !== undefined && resultSet[0].COUNT !== null) {
+                return resultSet[0].COUNT;
+            } else if (resultSet[0].count !== undefined && resultSet[0].count !== null) {
+                return resultSet[0].count;
+            }
+        }
+        return 0;
+    }
+
+    private async triggerEvent(data: ProductSetEntityEvent | ProductSetUpdateEntityEvent) {
+        const triggerExtensions = await extensions.loadExtensionModules("codbex-products-Products-ProductSet", ["trigger"]);
+        triggerExtensions.forEach(triggerExtension => {
+            try {
+                triggerExtension.trigger(data);
+            } catch (error) {
+                console.error(error);
+            }            
+        });
+        producer.topic("codbex-products-Products-ProductSet").send(JSON.stringify(data));
+    }
+}
