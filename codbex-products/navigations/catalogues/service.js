6fa7286ad6288af4eb12a58b54828f6ba1a603e5
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-03-28 11:12:44 +0200

    Update service.js

--- codbex-products/navigations/catalogues/service.js
+++ codbex-products/navigations/catalogues/service.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'catalogues-navigation',
     label: "Catalogues",
     group: "products",
-    order: 3800,
+    order: 200,
     link: "/services/web/codbex-products/gen/codbex-products/ui/Catalogues/index.html?embedded"
 };
 
